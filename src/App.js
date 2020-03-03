2d80d2f1e29e86baff5be95e6c6baf7bcea3f5a1
vellkunio <vellkunio>
2020-03-03 00:37:28 -0500

    s

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import Form from "./components/Form"
 import Weather from "./components/Weather"
 
 const API_KEY = "a76efa0956d43ba14872707af0aa81be";
-let city = "London";
 
 class App extends React.Component
 {
 
   gettingWeather = async (e) => {
     e.preventDefault();
-
-    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    const CITY = e.target.elements.city.value;
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}`);
     const data = await api_url.json();
     console.log(data);
 
